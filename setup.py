b6c6c0a973573bf052f50147ec3589c3962ff8b2
thedemons <thedemonsenc@gmail.com>
2022-01-13 21:09:04 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,22 +8,22 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
-# setup(
-#     name="opentele",
-#     version="1.0.3",
-#     description="None",
-#     long_description=README,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/realpython/reader",
-#     author="thedemons",
-#     author_email="thedemons@gmail.com",
-#     license="MIT",
-#     classifiers=[
-#         "License :: OSI Approved :: MIT License",
-#         "Programming Language :: Python :: 3",
-#         "Programming Language :: Python :: 3.7",
-#     ],
-#     packages=["opentele"],
-#     include_package_data=True,
-#     install_requires=["pyqt5", "telethon"],
-# )
+setup(
+    name="opentele",
+    version="1.0.5",
+    description="None",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thedemons/opentele",
+    author="thedemons",
+    author_email="thedemons@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["src"],
+    include_package_data=True,
+    install_requires=["pyqt5", "telethon", "tgcrypto"],
+)
