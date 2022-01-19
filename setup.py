14aece3f77949e6507a497c12c219ff937b8cbf4
thedemons <thedemonsenc@gmail.com>
2022-01-19 14:43:54 +0700

    Update requirements

--- setup.py
+++ setup.py
@@ -5,26 +5,45 @@ import re
 README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 PACKAGE_NAME        = "opentele"
-VERSION             = "1.0.8.2"
+VERSION             = "1.0.9"
 SOURCE_DIRECTORY    = "src"
 
+with open("requirements.txt") as data:
+    requirements = [
+        line for line in data.read().split("\n")
+        if line and not line.startswith("#")
+    ]
+
 setup(
+
     name=PACKAGE_NAME,
     version=VERSION,
+    license="MIT",
+
     description="A python library created to make life easier for Telegram API Developers.",
     long_description=README,
     long_description_content_type="text/markdown",
+    
     url="https://github.com/thedemons/opentele",
     author="thedemons",
     author_email="thedemons@gmail.com",
-    license="MIT",
+    
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
+    
+    kerwords=[
+        "tdata",
+        "tdesktop",
+        "telegram",
+        "telethon",
+        "opentele",
+    ],
+    
     packages=["opentele", "opentele.td", "opentele.tl"],
     package_dir={'opentele': 'src'},
     include_package_data=True,
-    install_requires=["pyqt5", "telethon", "tgcrypto"],
+    install_requires=requirements,
 )
