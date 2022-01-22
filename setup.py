cc78b95c0fe67c9d6a79f8f2762a7d625a246340
thedemons <thedemonsenc@gmail.com>
2022-01-22 21:29:22 +0700

    Remove micro version from overall version name
    Change version name from v1.1.3 to v1.13, for future patches (e.g. v1.13.1)

--- setup.py
+++ setup.py
@@ -4,37 +4,31 @@ import re
 
 README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
-PACKAGE_NAME        = "opentele"
-VERSION             = "1.1.3"
-SOURCE_DIRECTORY    = "src"
+PACKAGE_NAME = "opentele"
+VERSION = "1.13"
+SOURCE_DIRECTORY = "src"
 
 with open("requirements.txt") as data:
     requirements = [
-        line for line in data.read().split("\n")
-        if line and not line.startswith("#")
+        line for line in data.read().split("\n") if line and not line.startswith("#")
     ]
 
 setup(
-
     name=PACKAGE_NAME,
     version=VERSION,
     license="MIT",
-
     description="A python library created to make life easier for Telegram API Developers.",
     long_description=README,
     long_description_content_type="text/markdown",
-    
     url="https://github.com/thedemons/opentele",
     author="thedemons",
     author_email="thedemons@gmail.com",
-    
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Development Status :: 5 - Production/Stable",
     ],
-    
     keywords=[
         "tdata",
         "tdesktop",
@@ -42,9 +36,8 @@ setup(
         "telethon",
         "opentele",
     ],
-    
-    packages=[PACKAGE_NAME] + find_packages(PACKAGE_NAME),
-    package_dir={PACKAGE_NAME: SOURCE_DIRECTORY},
     include_package_data=True,
+    packages=[PACKAGE_NAME],
+    package_dir={PACKAGE_NAME: SOURCE_DIRECTORY},
     install_requires=requirements,
 )
